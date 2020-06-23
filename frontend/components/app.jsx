2e52990c031368fdab05cb1f9645898f61da4afb
John Lam <johnlamll916@gmail.com>
2020-06-23 12:28:01 -0700

    added nav for splash, signin and signup container and forms

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Route, Switch, Link, HashRouter} from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import Splash from './splash/splash';
+import SignupFormContainer from './session/signin_form_container';
+import SigninFormContainer from './session/login_form_container';
+
 const App = () => {
     return (
         <div>
-            <Splash />
+            <Splash/>
+            <Switch>
+                <AuthRoute exact path="/login" component={SignupFormContainer} />
+                <AuthRoute exact path="/signup" component={SigninFormContainer} />
+            </Switch>
         </div>
-        // <Route path="/login" component={LoginFormContainer} />
-        // <Route path="/signup" component={SigninFormContainer} />
     )
 }
 
