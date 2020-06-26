4f410e75aaad7ff7769c0d88327134c399498f80
John Lam <johnlamll916@gmail.com>
2020-06-25 18:03:56 -0700

    refactored some code on nav

--- frontend/components/dashboard/dashboard_container.jsx
+++ frontend/components/dashboard/dashboard_container.jsx
@@ -4,9 +4,25 @@ import { signout, signin } from '../../actions/session_actions';
 
 
 const Dashboard = (props) => {
+    //example
+    let feedArr = []
+    for (let i = 1; i <= 10; i++ ) {
+        feedArr.push(<li className="feed" key={`${i}`}>{`feed ${i}`}</li>);
+    }
+
     return (
-        <div>
-           <h1> welcome {props.currentUser.username} </h1>
+        <div className="dashboard-container">
+            <div className="dash-container-left">
+                <h1> welcome {props.currentUser.username} </h1>
+            </div>
+            <div className="dash-container-mid">
+                <div className="feed-container">
+                        <ul className="feeds-list">
+                            {feedArr}
+                        </ul>
+                </div>
+            </div>
+            <div className="dash-container-right"></div>
         </div>
     )
 }
