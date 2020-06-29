e7a16b53488367fe038e1134b312e6f81126fa46
John Lam <johnlamll916@gmail.com>
2020-06-29 11:22:31 -0700

    nav bar looking good

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+
+
 class Nav extends React.Component {
 
 
     sessionLinks () {
         return (
             <header className="nav-header">
-                <nav className="container nav-content">
+                <nav className="container">
                     <div className="nav-header-logo">
                         <a className="striva-logo" href="#">STRIVA</a>
                     </div>
@@ -30,15 +32,15 @@ greetUser() {
                         <a className="striva-logo nav-header-logo" href='#'>STRIVA</a>
                         <div className="nav-bar-main">
                             <ul className="nav-bar-list"> 
-                                <li><NavLink to="/dashboard">Dashboard<strong className="downarrow">⌄</strong></NavLink>
+                                <li className="top-bar"><NavLink to="/dashboard" activeClassName="nav-link-active">Dashboard<strong className="downarrow">⌄</strong></NavLink>
                                     <ul className="dashboard-list-hide">
-                                        <li><NavLink to="/dashboard">Activity Feed</NavLink></li>
-                                        <li><NavLink to="/routes">My Routes</NavLink></li>
+                                        <li><NavLink activeClassName="nav-link-active-bold" className="nav-link" to="/dashboard">Activity Feed</NavLink></li>
+                                        <li><NavLink activeClassName="nav-link-active-bold" to="/routes">My Routes</NavLink></li>
                                     </ul>
                                 </li>
-                                <li><NavLink to="/activities">Activities<strong className="downarrow">⌄</strong></NavLink>
+                                <li className="top-bar"><NavLink className="nav-link" activeClassName="nav-link-active" to="/activities">Activities<strong className="downarrow">⌄</strong></NavLink>
                                     <ul className="dashboard-list-hide">
-                                        <li><NavLink to="/activies">My Activities</NavLink></li>
+                                        <li><NavLink to="/activies" activeClassName="nav-link-active-bold">My Activities</NavLink></li>
                                     </ul>
                                 </li>
                             </ul>
