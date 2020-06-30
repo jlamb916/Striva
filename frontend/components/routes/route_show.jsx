cbb497538ee8d36bd866d9edf8765151493f1736
John Lam <johnlamll916@gmail.com>
2020-06-29 23:06:57 -0700

    convert miles, time, date to the correct format

--- frontend/components/routes/route_show.jsx
+++ frontend/components/routes/route_show.jsx
@@ -11,6 +11,24 @@ class RouteShow extends React.Component {
         if (this.props.route === undefined) {
             this.props.fetchRoute(this.props.routeId);
         }
+        if (this.props.route) {
+            this.props.fetchUser
+        }
+    }
+
+    convertTime (timestamp) {
+        let hours = Math.floor(timestamp / 60 / 60);
+        let minutes = Math.floor(timestamp / 60) - (hours * 60);
+        let seconds = Math.floor(timestamp % 60);
+        let formatted = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
+        return formatted
+    }
+
+    convertDate (timeStr) {
+        let splitStr = timeStr.split("T");
+        let dates = splitStr[0].split("-")
+        let newDate = new Date(dates[0], dates[1], dates[2]);
+        return newDate.toDateString();
     }
 
     render () {
@@ -19,26 +37,38 @@ class RouteShow extends React.Component {
         let routeData;
         let routeDuration;
         let routeDistance;
+        let movingTime; 
+        let miles;
+        let createdDate;
         if (route) {
             routeData = JSON.parse(route.route_data);
             routeDuration = routeData.matchings[0].duration;
             routeDistance = routeData.matchings[0].distance;
+            miles = (routeDistance / 1609).toFixed(2);
+            movingTime = this.convertTime(routeDuration);
+            createdDate = this.convertDate(route.created_at)
         }
         let renderData;
         renderData = routeData ?
              (<div className="route-show-data">
-                <h3 className="route-name">{route.route_name}</h3>
+                <div className="user-info">
+                    <ul>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
                 <ul>
                     <li>
-                        Distance: {routeDistance}
+                        Distance: {miles} mi
                     </li>
                 </ul>
                 <div className="route-moving-time">
-                    Est. Moving Time {routeDuration}
+                    Est. Moving Time {movingTime}
                 </div>
                 <div className="route-timestamp">
-                    <p>{route.created_at}</p>
+                    <p>{createdDate}</p>
                 </div>
+                <h3 className="route-name">{route.route_name}</h3>
             </div>) : "";
 
         {if (route) {
@@ -46,12 +76,6 @@ class RouteShow extends React.Component {
         <div className="route-container">
                 <div>
                     <h5><Link className="my-routes-link" to="/routes">My Routes</Link> / {route.route_name}</h5>
-                </div>
-                <div className="user-info">
-                    <ul>
-                        <li>By </li>
-                        <li></li>
-                    </ul>
                 </div>
                 <div className="route-title">
                     <h1>{route.route_name}</h1>
