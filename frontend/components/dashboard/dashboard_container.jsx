e24360ec56442fb9e182a2c755c19c4d32348d3f
John Lam <johnlamll916@gmail.com>
2020-08-07 14:26:16 -0700

    add links

--- frontend/components/dashboard/dashboard_container.jsx
+++ frontend/components/dashboard/dashboard_container.jsx
@@ -15,7 +15,31 @@ const Dashboard = (props) => {
             <div className="dash-container-mid">
                 <FeedContainer />
             </div>
-            <div className="dash-container-right"></div>
+            <div className="dash-container-right">
+                <div className="links-container">
+                    <div className="link">
+                        <div className="github-image-feed"></div>
+                        <div className='link1'>
+                            <div className="link-title">Github</div>
+                            <a target="_target" href="https://github.com/jlamb916/jumpman">View my Github</a>
+                        </div>
+                    </div>
+                    <div className="link">
+                        <div className="linkedin-image-feed"></div>
+                        <div className='link1'>
+                            <div className="link-title">LinkedIn</div>
+                            <div><a target="_target" href="https://linkedin.com/in/johnlam916">View my LinkedIn</a></div>
+                        </div>
+                    </div>
+                    <div className="link">
+                        <div className="angel-image-feed"></div>
+                        <div className='link1'>
+                            <div className="link-title">AngelList</div>
+                            <div ><a target="_target" href="https://angel.co/u/john-lam-17">View my AngelList</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
