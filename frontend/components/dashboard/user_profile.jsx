e08dbc4d780f5575285fe0ad8b9782dc94127fec
John Lam <johnlamll916@gmail.com>
2020-08-11 17:09:56 -0700

    user-dashboard profile card

--- frontend/components/dashboard/user_profile.jsx
+++ frontend/components/dashboard/user_profile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {NavLink} from 'react-router-dom';
 //get user followers and followings
 class UserProfile extends React.Component {
     constructor(props) {
@@ -17,18 +17,24 @@ class UserProfile extends React.Component {
                     <h2 className="dash-username"> {user.username}</h2>
                 </div>
                 <div className="profile-stats">
-                    {/* <ul>
-                        <li>Following</li>
-                        <li>0</li>
-                    </ul>
-                    <ul>
-                        <li>Follwers</li>
-                        <li>0</li>
-                    </ul>
-                    <ul>
-                        <li>Activities</li>
-                        <li>0</li>
-                    </ul> */}
+                    <div className="profile-activity-data">
+                        <div className='profile-column-feed profile-right'>
+                            <p className="stat-label-feed" >Routes</p>
+                            <p className="stat-data">0</p>
+                        </div>
+
+                        <div className='profile-column-feed'>
+                            <p className="stat-label-feed" >Activities</p>
+                            <p className="stat-data">0</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='lastest-activity'>
+                    <p className="latest-activity-title">Latest Activity</p>
+                    <NavLink className="profile-acitivity-link"to={`/activity/`}>  <p><span className="run-type">Morning Run ·</span> activity</p></NavLink>
+                </div>
+                <div className="recent-activities">
+                    <NavLink className="profile-acitivity-link" to={`/activity/`}><p className='training-log'>Your Training Log</p></NavLink>
                 </div>
             </div>
             
