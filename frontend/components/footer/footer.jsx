9a39e12fedbc396ee9e2d9426e95ecb075428c38
John Lam <johnlamll916@gmail.com>
2020-09-14 21:31:59 -0700

    fixed footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <h2 className="footer-text">STRIVA</h2>
-        </footer>
+      <footer className="footer">
+        <h2 className="footer-text">STRIVA</h2>
+        <h3>© 2020 Striva</h3>
+      </footer>
     );
 }
 
